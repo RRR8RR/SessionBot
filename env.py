65a0c4de6af2a8fe3eb8c035edec1ed3ac386691
RRR8RR <139396961+RRR8RR@users.noreply.github.com>
2023-08-01 15:27:20 +0300

    Add files via upload

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "19645327").strip()
-API_HASH = os.getenv("API_HASH", "92de937beb2f87db08df95bcca0ac2d6").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "5801830106:AAHVe5BbKsJFNF9sRYSPIVLr80m0tCs_uU0").strip()
+API_ID = os.getenv("API_ID", "27744530").strip()
+API_HASH = os.getenv("API_HASH", "a3346f334b802f16ad969f432e47291c").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6271013981:AAGcOwlTyhS7o81qldTie2xncsL3EcZMJjQ").strip()
 DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/Tepthon")
+MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/xLxLxLrr")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
